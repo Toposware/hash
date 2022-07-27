5dbf6f15d8437ccf241ece55dae15279a7050caf
Robin Salen <salenrobin@gmail.com>
2022-07-27 17:52:53 -0400

    Change MDS matrix for a circulant MDS one

--- src/anemoi/anemoi_64_12_8/mds.rs
+++ src/anemoi/anemoi_64_12_8/mds.rs
@@ -7,44 +7,20 @@
 // except according to those terms.
 
 use super::NUM_COLUMNS;
-use cheetah::Fp;
 
 /// Maximum Diffusion Layer matrix for Anemoi,
-pub(crate) const MDS: [Fp; NUM_COLUMNS * NUM_COLUMNS] = [
-    Fp::new(7),
-    Fp::new(7683234439643377518),
-    Fp::new(4969464316397294165),
-    Fp::new(1486652383518389446),
-    Fp::new(10374160003859427328),
-    Fp::new(2080639838189532388),
-    Fp::new(49),
-    Fp::new(16889152938674473991),
-    Fp::new(5575996515595268031),
-    Fp::new(15376031001026020287),
-    Fp::new(318796132876043458),
-    Fp::new(6491894801771569723),
-    Fp::new(2401),
-    Fp::new(15911754940807515141),
-    Fp::new(5868215007390030733),
-    Fp::new(17435674188209071342),
-    Fp::new(13491306927794819838),
-    Fp::new(13001985994131448344),
-    Fp::new(5764801),
-    Fp::new(916645121239609502),
-    Fp::new(7270777467373168997),
-    Fp::new(17628207846428173141),
-    Fp::new(11665471366669951938),
-    Fp::new(10255705198543237153),
-    Fp::new(33232930569601),
-    Fp::new(3958698637022038721),
-    Fp::new(17681628264411007595),
-    Fp::new(9238126393220571964),
-    Fp::new(982080019696549145),
-    Fp::new(2867565295223319292),
-    Fp::new(3732854072722565977),
-    Fp::new(11250920084865090207),
-    Fp::new(5482877217478222584),
-    Fp::new(1482022857694203904),
-    Fp::new(16108729179239576902),
-    Fp::new(17911738215939738154),
+/// This matrix is the first MDS matrix being outputted by this exhaustive
+/// SageMath search among all circulant matrices:
+///
+/// sage: from itertools import combinations_with_replacement
+/// sage: for l in combinations_with_replacement(range(0,7), 6):
+///           if is_mds(matrix.circulant(list(l))):
+///               print(i)
+///               break
+// Because of the small sizes of the coefficients of the matrix,
+// those are stored as u32 to enjoy faster multiplication with
+// `Fp` elements.
+pub(crate) const MDS: [u32; NUM_COLUMNS * NUM_COLUMNS] = [
+    1, 1, 3, 4, 5, 6, 6, 1, 1, 3, 4, 5, 5, 6, 1, 1, 3, 4, 4, 5, 6, 1, 1, 3, 3, 4, 5, 6, 1, 1, 1, 3,
+    4, 5, 6, 1,
 ];
